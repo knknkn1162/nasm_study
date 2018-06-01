c2825f80f30e65286a0bdfb204b39ae53d446f41
knknkn <knknkn1162@gmail.com>
2018-06-01 10:28:31 +0900

    add conditional1.asm

---
+++ ubuntu/conditional1.asm
@@ -0,0 +1,29 @@
+section .data
+	num1: equ 100
+	num2: equ 50
+	msg: db "Sub is correct", 10
+
+
+section .text
+  global _start
+
+_start:
+  mov rax, num1
+  mov rbx, num2
+  add rax, rbx
+  cmp rax, 150
+  jne .exit
+  jmp .rightSum
+
+.rightSum:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg
+  mov rdx, 15 ; count
+  syscall
+  jmp .exit
+
+.exit:
+  mov rax, 60
+  mov rdi, 0
+  syscall
