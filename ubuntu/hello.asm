f57be1ae96682af08d6424c56c8d028950abce25
knknkn <knknkn1162@gmail.com>
2018-06-04 15:50:53 +0900

    add times directive in hello.asm

--- ubuntu/hello.asm
+++ ubuntu/hello.asm
@@ -1,6 +1,7 @@
 ; global, section, db are directives which control the translation process.
 section .data ; hold global variables
-  text db "hello world", 10
+  text db "hello world", 10 ; db: create byte data
+  ex2 times 5 db 49 ; store "11111" string
 
 section .text ; hold instructions
   global _start
@@ -10,6 +11,11 @@ _start: ; entry point
   mov rdi, 1
   mov rsi, text
   mov rdx, 13
+  syscall
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, ex2
+  mov rdx, 5
   syscall
 
   mov rax, 60 ; exit
