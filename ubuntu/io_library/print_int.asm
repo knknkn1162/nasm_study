128b27778b852ff17cad07feb5d52dfa366bd021
knknkn <knknkn1162@gmail.com>
2018-06-06 10:09:27 +0900

    implement print_uint

--- ubuntu/io_library/print_int.asm
+++ ubuntu/io_library/print_int.asm
@@ -1,3 +1,6 @@
+section .data
+  minus db '-'
+
 section .bss
   num resb 50
 
@@ -11,6 +14,51 @@ _start:
   call exit
 
 print_int:
+  test rdi, 0x8000000000000000
+  jne .unsigned
+  ;print minus
+  mov rbx, rdi ; tmp-copy
+  mov rdi, minus
+  call print_char
+  neg rbx
+  mov rdi, rbx
+.unsigned:
+  call print_uint
+  ret
+
+print_char:
+  mov rax, 1
+  mov rsi, rdi
+  mov rdi, 1
+  mov rdx, 1
+  syscall
+  ret
+
+
+print_uint:
+  mov r8, num ; store the str pointer
+  mov rax, rdi
+  mov rcx, 10
+.loop:
+  xor rdx, rdx ; we must initialize rdx. without this, segmentation fault occurs
+  div rcx ;rax:rdx
+  add rdx, 48
+  mov [r8], dl
+  inc r8 ; increment address
+  cmp rax, 0
+  jne .loop
+.print:
+  mov rax, 1
+  mov rdx, 1 ;count
+  mov rdi, 1
+  mov rsi, r8
+.print_loop:
+  syscall
+  cmp rsi, num
+  je .end
+  dec rsi ; decrement address
+  jmp .print_loop
+.end:
   ret
 
 exit:
